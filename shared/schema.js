8b8365e3e44af965ef89a6cd3cbe59c2ff252d63
satish3130 <tsatish086@gmail.com>
2021-12-14 21:34:44 +0530

    schemaupdated

--- shared/schema.js
+++ shared/schema.js
@@ -4,11 +4,11 @@ const schema = {
   registerSchema: joi.object({
     name: joi.string().required(),
     email: joi.string().email().required(),
-    password: joi.string().min(5).max(12).required(),
+    password: joi.string().min(4).max(12).required(),
   }),
   loginSchema: joi.object({
     email: joi.string().email().required(),
-    password: joi.string().min(5).max(12).required(),
+    password: joi.string().min(4).max(12).required(),
   }),
   forgotSchema: joi.object({
     email: joi.string().email().required(),
